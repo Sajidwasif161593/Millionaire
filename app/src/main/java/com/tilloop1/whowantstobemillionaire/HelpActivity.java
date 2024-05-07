605be86bbad00a574d7560f392123af410f7017e
Wasif <sajid.wasif.sw@gmail.com>
2024-05-07 14:39:42 +0600

    first commit

---
+++ app/src/main/java/com/tilloop1/whowantstobemillionaire/HelpActivity.java
@@ -0,0 +1,30 @@
+package com.tilloop1.whowantstobemillionaire;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+
+ /* Class HelpActivity shows the Levels of the game and Prize money at each level*/
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+        getSupportActionBar().hide();
+    }
+
+
+//     * Function that is called when HOME button
+//     * @param view: The view that called the button
+
+    public void Home(View view){
+        Intent i =  new Intent(this, HomeActivity.class);
+        startActivity(i);
+    }
+}
+
